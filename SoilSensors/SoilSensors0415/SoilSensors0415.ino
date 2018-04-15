3e268c6c06a40b59ebf1ee2d07be5fa6bfe19b91
Jyneda <jyneda@gmail.com>
2018-04-15 22:11:31 +0800

    Add files via upload

---
+++ SoilSensors/SoilSensors0415/SoilSensors0415.ino
@@ -0,0 +1,47 @@
+#include <Bridge.h>
+
+char pin13[2];
+char valueStr[23];
+int valueInt;
+
+void setup() {
+    pinMode(13,OUTPUT);
+    
+    pinMode(A0,INPUT);
+    pinMode(A1,INPUT);
+    pinMode(A2,INPUT);
+    pinMode(A3,INPUT);
+    pinMode(A4,INPUT);
+    pinMode(A5,INPUT);
+    pinMode(A6,INPUT);
+    pinMode(A7,INPUT);
+
+    Bridge.begin();   // Pins 0 and 1 should be avoided as they are used by the Bridge library.
+}
+
+void loop() {
+    Bridge.get("Reg_done",  pin13, 2);
+    digitalWrite(13, atoi(pin13));    
+
+    char Moisture[]="Moisture1";
+    for (int i=0; i<=3; i++){
+        valueInt = analogRead(i);
+        itoa(valueInt, valueStr, 10); 
+        Moisture[8] = i + 49;
+        Bridge.put(Moisture, valueStr);
+        delay(500);
+    }
+
+    char pH[]="pH1";
+    for (int i=4; i<=7; i++){
+        valueInt = analogRead(i);
+        itoa(valueInt, valueStr, 10); 
+        pH[2] = i + 45;
+        Bridge.put(pH, valueStr);
+        delay(500);
+    }    
+
+   
+
+    
+}
---
+++ SoilSensors/custom.py
@@ -0,0 +1,23 @@
+import DAN
+
+ServerIP='210.61.8.56'
+Comm_interval = 10 # unit:second
+
+def profile_init():
+    DAN.profile['dm_name']='SoilSensors'
+    DAN.profile['d_name']='Soil-3' #DAN.get_mac_addr()[-4:]
+
+def odf():  # int only
+    return []
+
+def idf():
+    return [
+       ('Moisture1', float),
+       ('Moisture2', float),
+       ('Moisture3', float),
+       ('Moisture4', float),
+       ('pH1', float),
+       ('pH2', float),
+       ('pH3', float),
+       ('pH4', float),
+    ]
